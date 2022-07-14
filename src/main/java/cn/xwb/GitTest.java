29dc84392f7a59bcf32674f15030d3ef049cf4ad
xwb <x15871494329@163.com>
2022-07-14 11:29:26 +0800

    xwb第四次提交

--- src/main/java/cn/xwb/GitTest.java
+++ src/main/java/cn/xwb/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello test");
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("push test");
     }
 }
